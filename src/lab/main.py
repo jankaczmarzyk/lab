7a6419b045d20a0b282c46d08d7fe322622e7c27
Jan Kaczmarzyk <jan.kaczmarzyk@uekat.pl>
2026-09-03 15:28:00 +0200

    lab #04

--- src/lab/main.py
+++ src/lab/main.py
@@ -2,9 +2,9 @@ import pandas as pd
 info: str = pd.__version__
 
 def main() -> None:
-    print("-" * 60)
+    print("-" * 80)
     print(f"pandas version: {info}")
-    print("-" * 60)
+    print("-" * 80)
 
 if __name__ == "__main__":
     main()
